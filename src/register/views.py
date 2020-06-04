45a8f9aa9548d2b65949da932aa47e017bfa8919
Julián Novoa <julian.novoa@gmail.com>
2020-06-04 21:44:20 +0200

    Add copyright and reformat code

--- src/register/views.py
+++ src/register/views.py
@@ -1,5 +1,19 @@
-# views.py
-from django.shortcuts import render, redirect
+#  Copyright 2020 Julián Novoa Martín
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+from django.shortcuts import render
+
 from .forms import RegisterForm
 
 
@@ -9,9 +23,9 @@ def register_view(request):
         form = RegisterForm(request.POST)
         print(form.is_valid())
         if form.is_valid():
-            form.save()
-            return redirect("/")
+            usr = form.save()
+            return render(request, "register/successfulRegister.html", context={'user': usr})
     else:
         form = RegisterForm()
 
-    return render(request, "register/register.html", {"form":form})
+    return render(request, "register/register.html", {"form": form})
