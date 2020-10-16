481edc529c86581f61e4e148c63b3e362a7a27bc
Julián Novoa <julian.novoa@gmail.com>
2020-10-16 16:20:53 +0200

    Migrate to MariaDB

--- src/SmartScribble/settings.py
+++ src/SmartScribble/settings.py
@@ -83,8 +83,15 @@ WSGI_APPLICATION = 'SmartScribble.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smartscribbledb',
+        'USER': 'django',
+        'PASSWORD': 'SmartMariaScribbleDB2000!',
+        'HOST': 'localhost',
+        'PORT': '',
+        'TEST': {
+            'NAME': 'test_smartscribbledb',
+        },
     }
 }
 
