e631bf38601f974f31d2f2904a37c07b6116defd
Julián Novoa <julian.novoa@gmail.com>
2020-12-24 16:55:15 +0100

    Preload models in prod

--- src/document/apps.py
+++ src/document/apps.py
@@ -1,5 +1,18 @@
+import logging
+import sys
+
 from django.apps import AppConfig
 
 
 class DocumentConfig(AppConfig):
     name = 'document'
+
+    def ready(self):
+        from register.models import PredictionModels
+        from document.prediction import PredictionService
+
+        if 'test' not in sys.argv:
+            logger = logging.getLogger("DocumentConfig")
+            for model in PredictionModels:
+                logger.info(f"Preloading model {model.name}")
+                PredictionService.instance(model.name)
